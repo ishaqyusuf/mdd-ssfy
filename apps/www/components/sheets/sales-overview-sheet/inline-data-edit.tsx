bc04ade8f0bbe5ebefb0a0e755f01c817fcf8ad2
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-05-05 21:17:06 +0100

    :tada:

--- apps/www/components/sheets/sales-overview-sheet/inline-data-edit.tsx
+++ apps/www/components/sheets/sales-overview-sheet/inline-data-edit.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { updateSalesMetaAction } from "@/actions/update-sales-meta-action";
+import { revalidateTable } from "@/components/(clean-code)/data-table/use-infinity-data-table";
 import { DataSkeleton } from "@/components/data-skeleton";
 import { LabelInput } from "@/components/label-input";
 import { useDebounce } from "@/hooks/use-debounce";
@@ -14,18 +15,22 @@ export function SalesPO({ value, salesId }) {
     const hasMounted = useRef(false);
 
     useEffect(() => {
+        // console.log({ value, deb });
         if (!salesId) return;
         if (!hasMounted.current) {
             hasMounted.current = true;
             return;
         }
         if (value !== deb) {
+            console.log({ value, deb });
+
             updateSalesMetaAction(salesId, {
                 po: deb?.toUpperCase(),
             }).then(() => {
                 ctx.setParams({
                     refreshTok: generateRandomString(),
                 });
+                revalidateTable();
             });
         }
     }, [deb, value, salesId]);
