9181b438114cbbe19fd671e35860a9f835a114c2
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-05-22 17:54:56 +0100

    :tada:

--- apps/www/lib/open-link.ts
+++ apps/www/lib/open-link.ts
@@ -4,8 +4,9 @@ import QueryString from "qs";
 
 export function openLink(path, query = {}, newTab = false) {
     const link = document.createElement("a");
-    let base = env.NEXT_PUBLIC_APP_URL;
-    base = path?.startsWith("http") ? null : `${base}/`;
+    // let base = env.NEXT_PUBLIC_APP_URL;
+    // base = path?.startsWith("http") ? null : `${base}/`;
+    let base = path?.startsWith("http") ? "" : `${window.location.origin}/`;
     if (newTab) link.target = "_blank";
     link.href = `${base || ""}${path}?${QueryString.stringify(query)}`;
     link.click();
