d3c69cbb3c9d79e27cf5671f1c767ba7e55e60a3
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-05-27 11:05:11 +0100

    :tada:

--- apps/www/components/auth-guard.tsx
+++ apps/www/components/auth-guard.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { Access, validateRules } from "./sidebar/links";
 import type { ReactNode } from "react";
-import { useSession } from "@/hooks/use-session";
-import { rndTimeout } from "@/lib/timeout";
-import { useAsyncMemo } from "use-async-memo";
+import { useSessionZusStore } from "@/hooks/use-session";
 
 interface Props {
     children?: ReactNode;
@@ -12,23 +10,21 @@ interface Props {
 }
 
 export function AuthGuard({ children, Fallback = null, rules }: Props) {
-    const session = useSession();
-    // const [__state, setState] = useState<"idle" | "valid" | "invalid">("idle");
-    const state = useAsyncMemo(async () => {
-        await rndTimeout();
-        if (!session) return;
-        const isValid = validateRules(
-            rules,
-            session.can,
-            session.userId,
-            session.role,
-        );
-        console.log({ isValid });
-
-        return isValid;
-    }, [session, rules]);
-    if (state === null) return <span>session not loaded </span>;
-    if (!state) return Fallback;
+    const { session } = useSessionZusStore();
+    const isValid = useMemo(
+        () =>
+            session
+                ? validateRules(
+                      rules,
+                      session?.can,
+                      session?.userId,
+                      session?.role,
+                  )
+                : undefined,
+        [rules, session],
+    );
+    if (isValid === undefined) return <span>session not loaded </span>;
+    if (!isValid) return Fallback;
 
     return children ?? null;
 }
--- apps/www/hooks/use-session.tsx
+++ apps/www/hooks/use-session.tsx
@@ -2,23 +2,54 @@
 import { getLoggedInProfile } from "@/actions/cache/get-loggedin-profile";
 import { rndTimeout } from "@/lib/timeout";
 import { AsyncFnType } from "@/types";
-import { createContextFactory } from "@/utils/context-factory";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { dotSet } from "@/app/(clean-code)/_common/utils/utils";
+import { FieldPath, FieldPathValue } from "react-hook-form";
+import { create } from "zustand";
 
-export const { Provider: SessionProvider, useContext: useSession } =
-    createContextFactory(() => {
-        const [session, setSession] = useState<
-            AsyncFnType<typeof getLoggedInProfile>
-        >(null as any);
-        const path = usePathname();
-        useEffect(() => {
-            if (session) return;
-            (async () => {
-                await rndTimeout();
-                const data = await getLoggedInProfile();
-                setSession(data);
-            })();
-        }, [path, session]);
-        return session;
-    });
+export function SessionProvider({ children }) {
+    const store = useSessionZusStore();
+    const path = usePathname();
+    useEffect(() => {
+        if (store.session) return;
+        (async () => {
+            await rndTimeout();
+            const data = await getLoggedInProfile();
+            store.reset({
+                session: data,
+            });
+        })();
+    }, [path, store]);
+    return <>{children}</>;
+}
+const data = {
+    session: null as AsyncFnType<typeof getLoggedInProfile>,
+};
+type Action = ReturnType<typeof funcs>;
+type Data = typeof data;
+type Store = Data & Action;
+export type ZusFormSet = (update: (state: Data) => Partial<Data>) => void;
+
+function funcs(set: ZusFormSet) {
+    return {
+        reset: (resetData) =>
+            set((state) => ({
+                ...data,
+                ...resetData,
+            })),
+        update: <K extends FieldPath<Data>>(k: K, v: FieldPathValue<Data, K>) =>
+            set((state) => {
+                const newState = {
+                    ...state,
+                };
+                const d = dotSet(newState);
+                d.set(k, v);
+                return newState;
+            }),
+    };
+}
+export const useSessionZusStore = create<Store>((set) => ({
+    ...data,
+    ...funcs(set),
+}));
