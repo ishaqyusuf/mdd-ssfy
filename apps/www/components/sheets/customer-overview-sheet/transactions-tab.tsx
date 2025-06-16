8c8d0e812264d3c64696de4761d4ed09439c008c
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-06-16 15:23:29 +0100

    :tada:

--- apps/www/components/sheets/customer-overview-sheet/transactions-tab.tsx
+++ apps/www/components/sheets/customer-overview-sheet/transactions-tab.tsx
@@ -6,6 +6,7 @@ import { revalidateTable } from "@/components/(clean-code)/data-table/use-infini
 import { CancelSalesTransactionAction } from "@/components/cancel-sales-transaction";
 import { DataSkeleton } from "@/components/data-skeleton";
 import { EmptyState } from "@/components/empty-state";
+import { CustomerTxDataTable } from "@/components/tables/sales-accounting/table.customer-transaction";
 import {
     DataSkeletonProvider,
     useCreateDataSkeletonCtx,
@@ -47,25 +48,7 @@ export function TransactionsTab({ accountNo, salesId }: Props) {
     });
     const data = skel?.data;
     const toast = useLoadingToast();
-    const cancelTx = useAction(cancelSalesPaymentAction, {
-        onSuccess(args) {
-            toast.display({
-                title: "Cancelled",
-                duration: 2000,
-                variant: "destructive",
-            });
-            revalidateTable();
-            skel?.load();
-            // onDelete?.();
-        },
-        onExecute(args) {
-            toast.display({
-                title: "Unable to complete",
-                duration: 2000,
-                variant: "error",
-            });
-        },
-    });
+
     return (
         <EmptyState
             empty={data?.status && data?.transactions?.length == 0}
@@ -77,67 +60,9 @@ export function TransactionsTab({ accountNo, salesId }: Props) {
             }
         >
             <DataSkeletonProvider value={skel}>
-                <Table className="table-sm">
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead>Date</TableHead>
-                            <TableHead>Description</TableHead>
-                            <TableHead>Status</TableHead>
-                            <TableHead></TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {skel
-                            .renderList(data?.transactions, 5, {})
-                            ?.map((tx, i) => (
-                                <TableRow key={i} className={cn("")}>
-                                    <TableCell>
-                                        <DataSkeleton pok="date">
-                                            <TCell.Date>
-                                                {formatDate(tx.createdAt)}
-                                            </TCell.Date>
-                                        </DataSkeleton>
-                                    </TableCell>
-                                    <TableCell>
-                                        <DataSkeleton pok="textLg">
-                                            <TCell.Secondary>
-                                                <span>{tx.checkNo}</span>
-                                            </TCell.Secondary>
-                                        </DataSkeleton>
-                                        <TCell.Secondary className="inline-flex gap-2">
-                                            <DataSkeleton pok="textSm">
-                                                <Badge
-                                                    variant="secondary"
-                                                    className="px-1 py-0 uppercase"
-                                                >
-                                                    {tx.paymentMethod}
-                                                </Badge>
-                                            </DataSkeleton>
-                                            <DataSkeleton pok="textSm">
-                                                {/* {!tx.receivedBy || (
-                                                    <>
-                                                        {" by "}
-                                                        {tx.receivedBy}
-                                                    </>
-                                                )} */}
-                                            </DataSkeleton>
-                                        </TCell.Secondary>
-                                    </TableCell>
-                                    <TableCell className="font-mono">
-                                        <DataSkeleton pok="textSm">
-                                            <TCell.Status status={tx.status} />
-                                        </DataSkeleton>
-                                    </TableCell>
-                                    <TableCell className="inline-flex justify-end items-center">
-                                        <CancelSalesTransactionAction
-                                            status={tx.status}
-                                            customerTransactionId={tx?.id}
-                                        />
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                    </TableBody>
-                </Table>
+                <div className="flex flex-col w-full overflow-auto">
+                    <CustomerTxDataTable data={data?.transactions || []} />
+                </div>
             </DataSkeletonProvider>
         </EmptyState>
     );
