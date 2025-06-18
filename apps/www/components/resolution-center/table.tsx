2979c96075648ef82919c04f1b8a9ddec5a93a34
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-06-18 09:40:40 +0100

    :tada:

---
+++ apps/www/components/resolution-center/table.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import React, { use } from "react";
+import { MiddaySearchFilter } from "@/components/midday-search-filter/search-filter";
+import { useLoadingToast } from "@/hooks/use-loading-toast";
+import { Table, TableBody } from "@gnd/ui/table";
+
+import { TableProvider } from "../tables";
+import { TableHeaderComponent } from "../tables/table-header";
+import { TableRow } from "../tables/table-row";
+
+import { useEmployeesParams } from "@/hooks/use-employee-params";
+import { PageFilterData } from "@/types/type";
+import { columns, Item } from "./columns";
+import { LoadMore } from "../tables/load-more";
+import FContentShell from "@/components/(clean-code)/fikr-ui/f-content-shell";
+import { useRolesParams } from "@/hooks/use-roles-params";
+import { _perm } from "@/components/sidebar/links";
+import { useTransactionOverviewModal } from "@/hooks/use-tx-overview-modal";
+import { SuperAdminGuard } from "@/components/auth-guard";
+import { Button } from "@gnd/ui/button";
+import Link from "next/link";
+import { isProdClient } from "@/lib/is-prod";
+
+type Props = {
+    data: Item[];
+    loadMore: (query) => Promise<any>;
+    pageSize: number;
+    nextMeta;
+    filterDataPromise;
+};
+
+export function DataTable({
+    data,
+    loadMore,
+    pageSize,
+    nextMeta,
+    filterDataPromise,
+}: Props) {
+    const { setParams, params } = useEmployeesParams();
+    const filterData: PageFilterData[] = filterDataPromise
+        ? use(filterDataPromise)
+        : [];
+    const role = useRolesParams();
+    const toast = useLoadingToast();
+    //   const deleteEmployee = useAction(deleteStudentAction, {
+    //     onSuccess(args) {
+    //       toast.success("Deleted!", {
+    //         variant: "destructive",
+    //       });
+    //     },
+    //     onError(e) {},
+    //   });
+    const txView = useTransactionOverviewModal();
+    return (
+        <TableProvider
+            args={[
+                {
+                    columns,
+                    data,
+                    nextMeta,
+                    loadMore,
+                    pageSize,
+                    setParams,
+                    params,
+                    tableMeta: {
+                        filterData,
+                        deleteAction(id) {
+                            //   deleteStudent.execute({
+                            //     studentId: id,
+                            //   });
+                        },
+                        rowClick(id, rowData) {
+                            txView.viewTx(rowData?.id);
+                        },
+                    },
+                },
+            ]}
+        >
+            <div className="flex flex-col gap-4">
+                <FContentShell>
+                    <div className="flex">
+                        <MiddaySearchFilter
+                            placeholder={"Search"}
+                            filterList={filterData}
+                        />
+                        <div className="flex-1"></div>
+                        <SuperAdminGuard>
+                            <Button disabled={isProdClient}>
+                                <Link href="/sales-book/accounting/resolution-center">
+                                    Resolution Center
+                                </Link>
+                            </Button>
+                        </SuperAdminGuard>
+                    </div>
+                </FContentShell>
+                <Table>
+                    <TableHeaderComponent />
+
+                    <TableBody>
+                        <TableRow />
+                    </TableBody>
+                </Table>
+                <LoadMore />
+            </div>
+        </TableProvider>
+    );
+}
