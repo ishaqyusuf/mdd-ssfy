a518fe5984fd13d00088d65486b1a8ed8d46fc87
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-06-27 18:43:00 +0100

    server-only

--- apps/gnd-backlog/src/app/tasks/page.tsx
+++ apps/gnd-backlog/src/app/tasks/page.tsx
@@ -1,6 +1,7 @@
-"use client";
+import { batchPrefetch, trpc } from "@/trpc/server";
 import { Button } from "@gnd/ui/button";
 export default async function Page() {
+  batchPrefetch([trpc.tasks.get.infiniteQueryOptions()]);
   return (
     <div>
       <Button>Submit</Button>
