d3fd65ef4a14e07a276d3e4a4ec51874068b7263
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-07-03 19:33:18 +0100

    customer page--

--- apps/www/app/(clean-code)/(sales)/sales-book/customers/page.tsx
+++ apps/www/app/(clean-code)/(sales)/sales-book/customers/page.tsx
@@ -7,7 +7,7 @@ import NewFeatureBtn from "@/components/common/new-feature-btn";
 import TablePage from "@/components/tables/table-page";
 import { constructMetadata } from "@/lib/(clean-code)/construct-metadata";
 
-import CustomersPageClient from "../_(pages)/_components/customers-page-client";
+// import CustomersPageClient from "../_(pages)/_components/customers-page-client";
 
 export async function generateMetadata({}) {
     return constructMetadata({
@@ -28,12 +28,12 @@ export default async function CustomersPage({ searchParams }) {
             <Portal nodeId={"navRightSlot"}>
                 <NewFeatureBtn href="/sales/customers">Old Site</NewFeatureBtn>
             </Portal>
-            {/* <CustomersPageClient {...props} /> */}
-            <TablePage
+
+            {/* <TablePage
                 PageClient={CustomersPageClient}
                 searchParams={searchParams}
                 filterKey="customers"
-            />
+            /> */}
         </FPage>
     );
 }
