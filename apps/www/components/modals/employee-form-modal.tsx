89e3d48fc990cc97cb058393fd7b965a37f7acb1
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-07-03 18:41:01 +0100

    asd

--- apps/www/components/modals/employee-form-modal.tsx
+++ apps/www/components/modals/employee-form-modal.tsx
@@ -1,1 +1,38 @@
-export function EmployeeFormModal({}) {}
+import { useEmployeesParams } from "@/hooks/use-employee-params";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+} from "@gnd/ui/dialog";
+import { Form } from "@gnd/ui/form";
+
+export function EmployeeFormModal({}) {
+    const { setParams, params, opened } = useEmployeesParams();
+
+    return (
+        <Dialog
+            open={opened}
+            onOpenChange={(e) => {
+                setParams(null);
+            }}
+        >
+            <DialogContent
+                onOpenAutoFocus={(evt) => evt.preventDefault()}
+                className="max-w-[xl]"
+            >
+                <div className="p-4">
+                    <DialogHeader className="mb-4">
+                        <DialogTitle>Employee Form</DialogTitle>
+                    </DialogHeader>
+                </div>
+                {/* <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+
+                    </form>
+                </Form> */}
+            </DialogContent>
+        </Dialog>
+    );
+}
+
--- apps/www/components/modals/global-modals.tsx
+++ apps/www/components/modals/global-modals.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SuperAdminGuard } from "../auth-guard";
+import { EmployeeFormModal } from "./employee-form-modal";
 import { InboundSalesModal } from "./inbound-status-modal";
 import { LaborCostModal } from "./labor-cost-modal";
 
@@ -11,6 +12,7 @@ export function GlobalModals() {
                 <LaborCostModal />
             </SuperAdminGuard>
             <InboundSalesModal />
+            <EmployeeFormModal />
         </>
     );
 }
