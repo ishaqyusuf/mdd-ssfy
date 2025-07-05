a18a989aa8d7814fb120ec7661402aa9b52a0283
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-07-05 17:10:54 +0100

    cleanups

--- apps/www/app/(download)/download/[...slugs]/page.tsx
+++ apps/www/app/(download)/download/[...slugs]/page.tsx
@@ -2,7 +2,6 @@ import { prisma } from "@/db";
 import SalesDownload from "./sales";
 
 export default async function DownloadPage({ params }) {
-    console.log(params.slugs);
     const [path, token, slug]: ["sales", string, string] = params.slugs;
 
     const order = await prisma.salesOrders.findFirst({
