dda650592f9c285ef5285259e9b489c3e57d01fd
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-07-08 14:41:17 +0100

    sadsad

--- apps/www/src/hooks/use-user.ts
+++ apps/www/src/hooks/use-user.ts
@@ -1,12 +1,10 @@
 import { useTRPC } from "@/trpc/client";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-
-
 export function useUserProfileUpdate() {
     const trp = useTRPC();
     const queryClient = useQueryClient();
-    return useMutation(
-        trpc.
-    )
+    // return useMutation(
+    //     trpc.
+    // )
 }
