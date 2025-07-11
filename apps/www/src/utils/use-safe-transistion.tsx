01941f34c7a6808dc8380b453ed8c13f1b0a66da
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-07-11 15:28:52 +0100

    asdas

--- apps/www/src/utils/use-safe-transistion.tsx
+++ apps/www/src/utils/use-safe-transistion.tsx
@@ -1,6 +1,6 @@
 import { useTransition as useBaseTransition } from "react";
 
-type AsyncFn = () => Promise<void>;
+type AsyncFn = () => Promise<void> | void;
 
 export function useTransition(): [boolean, (fn: AsyncFn) => void] {
     const [isPending, startTransition] = useBaseTransition();
