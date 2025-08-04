c9d6f3df0e35b5ac768f7c5f80f4fca1be3687f5
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-08-04 14:58:52 +0100

    asdas

--- apps/site/src/app/api/auth-session/route.ts
+++
@@ -1,7 +0,0 @@
-import { getLoggedInProfile } from "@/actions/cache/get-loggedin-profile";
-import { NextRequest } from "next/server";
-
-export async function POST(req: NextRequest) {
-    const response = await getLoggedInProfile();
-    return Response.json(response);
-}
