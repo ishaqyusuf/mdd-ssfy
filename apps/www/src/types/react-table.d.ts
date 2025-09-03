5663b336691a07c70833243e06ce8d62353eccfd
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-09-03 17:00:20 +0100

    asds

--- apps/www/src/types/react-table.d.ts
+++ apps/www/src/types/react-table.d.ts
@@ -1,38 +1,38 @@
-import type { RouterOutputs } from "@/trpc/client";
+// import type { RouterOutputs } from "@/trpc/client";
 
-declare module "@tanstack/table-core" {
-    interface TableMeta<TData extends RowData> {
-        // Transaction table meta
-        dateFormat?: string | null;
-        timeFormat?: number | null;
-        hasSorting?: boolean;
-        setOpen?: (id: string) => void;
-        copyUrl?: (id: string) => void;
-        updateTransaction?: (data: { id: string; status: string }) => void;
-        onDeleteTransaction?: (id: string) => void;
+// declare module "@tanstack/table-core" {
+//     interface TableMeta<TData extends RowData> {
+//         // Transaction table meta
+//         dateFormat?: string | null;
+//         timeFormat?: number | null;
+//         hasSorting?: boolean;
+//         setOpen?: (id: string) => void;
+//         copyUrl?: (id: string) => void;
+//         updateTransaction?: (data: { id: string; status: string }) => void;
+//         onDeleteTransaction?: (id: string) => void;
 
-        // Vault table meta
-        handleDelete?: (id: string) => void;
-        handleShare?: (pathTokens: string[]) => void;
+//         // Vault table meta
+//         handleDelete?: (id: string) => void;
+//         handleShare?: (pathTokens: string[]) => void;
 
-        // Categories table meta
-        deleteCategory?: (id: string) => void;
-        expandedCategories?: Set<string>;
-        setExpandedCategories?: React.Dispatch<
-            React.SetStateAction<Set<string>>
-        >;
+//         // Categories table meta
+//         deleteCategory?: (id: string) => void;
+//         expandedCategories?: Set<string>;
+//         setExpandedCategories?: React.Dispatch<
+//             React.SetStateAction<Set<string>>
+//         >;
 
-        // Customers table meta
-        deleteCustomer?: (id: string) => void;
+//         // Customers table meta
+//         deleteCustomer?: (id: string) => void;
 
-        // Members table meta
-        currentUser?: RouterOutputs["team"]["members"][number];
-        totalOwners?: number;
-    }
+//         // Members table meta
+//         currentUser?: RouterOutputs["team"]["members"][number];
+//         totalOwners?: number;
+//     }
 
-    // interface ColumnMeta<TData extends RowData, TValue> {
-    //     className?: string;
-    //     isHidden?: boolean;
-    // }
-}
+//     // interface ColumnMeta<TData extends RowData, TValue> {
+//     //     className?: string;
+//     //     isHidden?: boolean;
+//     // }
+// }
 
