453744f0065055d74601951217d28628c1d65ea8
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-09-05 12:39:34 +0100

    sda

--- apps/www/src/app/(sidebar)/(sales)/sales-book/orders/sales-statistics/page.tsx
+++ apps/www/src/app/(sidebar)/(sales)/sales-book/orders/sales-statistics/page.tsx
@@ -4,8 +4,8 @@ import { ErrorFallback } from "@/components/error-fallback";
 import { TableSkeleton } from "@/components/tables/skeleton";
 import FPage from "@/components/(clean-code)/fikr-ui/f-page";
 import { constructMetadata } from "@/lib/(clean-code)/construct-metadata";
-import { OrderHeader } from "@/components/sales-order-header";
 import { DataTable } from "@/components/tables/sales-statistics/data-table";
+import { SalesStatHeader } from "@/components/sales-stat-header";
 
 export async function generateMetadata(props) {
     return constructMetadata({
@@ -15,7 +15,7 @@ export async function generateMetadata(props) {
 export default async function Page() {
     return (
         <FPage can={["viewOrders"]} title="Sales Statistics">
-            <OrderHeader />
+            <SalesStatHeader />
             <ErrorBoundary errorComponent={ErrorFallback}>
                 <Suspense fallback={<TableSkeleton />}>
                     <DataTable />
