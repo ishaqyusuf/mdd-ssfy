1ec0875b67562f5f82cba75147d8204f42a2d794
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-09-12 17:33:24 +0100

    asdas

--- apps/www/src/components/sheets/sales-overview-sheet/general-action-bar.tsx
+++ apps/www/src/components/sheets/sales-overview-sheet/general-action-bar.tsx
@@ -18,15 +18,57 @@ import { cn } from "@gnd/ui/cn";
 import { openLink } from "@/lib/open-link";
 import { salesFormUrl } from "@/utils/sales-utils";
 import { useBatchSales } from "@/hooks/use-batch-sales";
+import { CheckCheck, FileSearch, RefreshCcw } from "lucide-react";
+import { useSalesPreview } from "@/hooks/use-sales-preview";
+import { MenuItemSalesActions } from "@/components/menu-item-sales-actions";
+import { AuthGuard } from "@/components/auth-guard";
+import { _perm } from "@/components/sidebar/links";
+import { useSalesOverviewQuery } from "@/hooks/use-sales-overview-query";
+import { useRef, useState, useTransition } from "react";
+import { resetSalesStatAction } from "@/actions/reset-sales-stat";
+
+import { useSalesPrintParams } from "@/hooks/use-sales-print-params";
+import { useLoadingToast } from "@/hooks/use-loading-toast";
+import { useSalesQueryClient } from "@/hooks/use-sales-query-client";
+import { toast } from "sonner";
 
 export function GeneralActionBar({ type, salesNo, salesId }) {
     const mailer = useSalesMailer();
     const { data } = useSaleOverview();
     const isQuote = data?.type == "quote";
     const batchSales = useBatchSales();
+    const sPreview = useSalesPreview();
+    function preview() {
+        sPreview.preview(data?.orderId, data?.type);
+    }
+    const [loading, startTransition] = useTransition();
+    const qs = useSalesOverviewQuery();
+
+    async function reset() {
+        startTransition(async () => {
+            try {
+                const resp = await resetSalesStatAction(
+                    data?.id,
+                    data?.orderId,
+                );
+                toast.success("Reset complete");
+                qs.salesQuery.salesStatReset();
+                // qs.setParams({
+                //     refreshTok: generateRandomString(),
+                // });
+            } catch (error) {
+                toast.error("Unable to complete");
+            }
+        });
+    }
+    const [menuOpen, setMenuOpen] = useState(false);
+    const printer = useSalesPrintParams();
+    const loader = useLoadingToast();
+    const sq = useSalesQueryClient();
+    const menuRef = useRef(null);
     return (
         <div className="flex gap-2">
-            <AlertDialog>
+            {/* <AlertDialog>
                 <AlertDialogTrigger asChild>
                     <Button
                         disabled
@@ -61,7 +103,16 @@ export function GeneralActionBar({ type, salesNo, salesId }) {
                         </AlertDialogAction>
                     </AlertDialogFooter>
                 </AlertDialogContent>
-            </AlertDialog>
+            </AlertDialog> */}
+            <Button
+                onClick={(e) => {}}
+                size="sm"
+                variant="default"
+                className="flex items-center space-x-2 hover:bg-secondary flex-1"
+            >
+                <FileSearch className="size-3.5" />
+                <span>Preview</span>
+            </Button>
             <Button
                 size="sm"
                 variant="secondary"
@@ -126,6 +177,7 @@ export function GeneralActionBar({ type, salesNo, salesId }) {
                             Payment Link
                         </Menu.Item>
                         <Menu.Item
+                            Icon={CheckCheck}
                             SubMenu={
                                 <>
                                     <Menu.Item
@@ -152,6 +204,38 @@ export function GeneralActionBar({ type, salesNo, salesId }) {
                         >
                             Mark as
                         </Menu.Item>
+                        <Menu.Separator />
+                        <MenuItemSalesActions
+                            slug={data?.uuid}
+                            menuRef={menuRef}
+                            setMenuOpen={setMenuOpen}
+                            id={data?.id}
+                            type={data?.type}
+                        />
+                        <Menu.Item
+                            Icon={RefreshCcw}
+                            onClick={reset}
+                            disabled={loading}
+                        >
+                            Reset Stats
+                        </Menu.Item>
+                        <AuthGuard rules={[_perm.is("viewSalesResolution")]}>
+                            <Menu.Item
+                                Icon={RefreshCcw}
+                                onClick={(e) => {
+                                    openLink(
+                                        `/sales-book/accounting/resolution-center`,
+                                        {
+                                            salesNo: data.orderId,
+                                        },
+                                        true,
+                                    );
+                                }}
+                                disabled={loading}
+                            >
+                                Resolution Center
+                            </Menu.Item>
+                        </AuthGuard>
                     </>
                 )}
             </Menu>
