cd16c44856951e500982e5aeaec5a21d06e2d4d1
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-09-16 14:07:13 +0100

    sada

--- apps/www/src/app/(public)/debug/layout.tsx
+++ apps/www/src/app/(public)/debug/layout.tsx
@@ -3,13 +3,15 @@ import Link from "@/components/link";
 
 export default async function DebugLayout({ children }) {
     return (
-        <Env isDev>
-            <div className="p-4 flex gap-4">
-                <Link href={"/debug/sheet"}>Sheet</Link>
-                <Link href={"/debug/command-input"}>Command</Link>
-                <Link href={"/debug/sales-stats"}>Sales Stats</Link>
-            </div>
+        <>
+            <Env isDev>
+                <div className="p-4 flex gap-4">
+                    <Link href={"/debug/sheet"}>Sheet</Link>
+                    <Link href={"/debug/command-input"}>Command</Link>
+                    <Link href={"/debug/sales-stats"}>Sales Stats</Link>
+                </div>
+            </Env>
             {children}
-        </Env>
+        </>
     );
 }
