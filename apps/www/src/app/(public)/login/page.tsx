6a1cab963313bc01f0192afd8d2c865d792a1d14
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-09-24 10:11:02 +0100

    asdas

--- apps/www/src/app/(public)/login/page.tsx
+++ apps/www/src/app/(public)/login/page.tsx
@@ -1,38 +1,20 @@
-import { Env } from "@/components/env";
-import { Icons } from "@/components/_v1/icons";
-import { LoginForm } from "@/components/login-form";
-import QuickLogin from "@/components/quick-login";
-import { Card, CardDescription, CardHeader, CardTitle } from "@gnd/ui/card";
+// import { Login01 } from "@/components/login-01";
+import { Login02 } from "@/components/login-02";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: "Login | GND",
 };
 export default async function Page() {
+    // return <Login01 />;
     return (
-        <div className="min-h-screen     flex items-center justify-center p-4">
-            <header className="absolute top-0 left-0 z-30 w-full">
-                <div className="p-6 md:p-8">
-                    <div className="h-8 w-auto">
-                        <Icons.logoLg />
-                    </div>
+        <>
+            <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+                <div className="w-full max-w-sm md:max-w-5xl">
+                    <Login02 />
                 </div>
-            </header>
-            <Card className="shadow-xl w-full max-w-md sborder-0">
-                <CardHeader className="space-y-1 pb-6">
-                    <CardTitle className="text-xl font-semibold text-center">
-                        Sign In
-                    </CardTitle>
-                    <CardDescription className="text-center">
-                        Enter your credentials to access your account
-                    </CardDescription>
-                </CardHeader>
-                <Env isDev>
-                    <QuickLogin />
-                </Env>
-                <LoginForm />
-            </Card>
-        </div>
+            </div>
+        </>
     );
 }
 
