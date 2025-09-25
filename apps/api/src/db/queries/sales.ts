65dffa3b369d3ea7193b283a37c8d3e8b17c3e25
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-09-25 20:02:04 +0100

    sad

--- apps/api/src/db/queries/sales.ts
+++ apps/api/src/db/queries/sales.ts
@@ -27,7 +27,7 @@ import {
   itemItemControlUid,
   mouldingItemControlUid,
 } from "@api/utils/sales-control";
-import { consoleLog, formatCurrency, timeLog } from "@gnd/utils";
+import { formatCurrency } from "@gnd/utils";
 import { calculateSalesDueAmount } from "@sales/sales-transaction";
 
 export async function getSales(
@@ -38,7 +38,6 @@ export async function getSales(
   if (query.defaultSearch) {
     if (query.showing != "all sales") query.salesRepId = ctx.userId!;
   }
-  consoleLog("QUERY", query);
   const { db } = ctx;
   const { response, searchMeta, where } = await composeQueryData(
     query,
