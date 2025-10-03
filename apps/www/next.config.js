33bfadb0afc4543273a520260d707a51fdd44459
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-03 11:14:28 +0100

    .

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,7 +1,8 @@
+import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const path = require("path");
 // const { NormalModuleReplacementPlugin } = require("webpack");
-const nextConfig = {
+const config = {
     transpilePackages: [
         // "@gnd/api",
         // "@gnd/auth",
@@ -11,6 +12,13 @@ const nextConfig = {
         "@gnd/printer",
         // "@gnd/stripe",
     ],
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    devIndicators: false,
     images: {
         // loader: "custom",
         // loaderFile: "./image-loader.ts",
@@ -91,4 +99,23 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+const isProduction = process.env.NODE_ENV === "production";
+
+export default isProduction
+    ? withSentryConfig(config, {
+          org: process.env.SENTRY_ORG,
+          project: process.env.SENTRY_PROJECT,
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+          telemetry: false,
+
+          // Only print logs for uploading source maps in CI
+          silent: !process.env.CI,
+
+          // Upload source maps for better stack traces
+          widenClientFileUpload: true,
+
+          // Tree-shake Sentry logger statements to reduce bundle size
+          disableLogger: true,
+      })
+    : config;
---
+++ apps/www/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c9379ca7d8c836c5cccaf67e3d2e4e61@o4503970060238848.ingest.us.sentry.io/4508013542244352",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
