e98963c49db5d54f03111183c1d7f452ad6d62a3
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-06 17:43:49 +0100

    .

--- apps/www/src/app/(sidebar)/(sales)/sales-book/(bebug)/layout.tsx
+++ apps/www/src/app/(sidebar)/(sales)/sales-book/(bebug)/layout.tsx
@@ -1,11 +1,23 @@
+import { buttonVariants } from "@gnd/ui/button";
+import { cn } from "@gnd/ui/cn";
 import Link from "next/link";
 
 export default async function Layout({ children }) {
+    const workingRoutes = [2];
     return (
         <div className="flex flex-col">
             <div className="">
                 {[...Array(3)].map((a, i) => (
-                    <Link href={`/sales-book/debug-${i + 1}`}>
+                    <Link
+                        className={cn(
+                            buttonVariants({
+                                variant: workingRoutes.includes(i + 1)
+                                    ? "link"
+                                    : "destructive",
+                            }),
+                        )}
+                        href={`/sales-book/debug-${i + 1}`}
+                    >
                         Debug {i + 1}
                     </Link>
                 ))}
@@ -14,3 +26,4 @@ export default async function Layout({ children }) {
         </div>
     );
 }
+
