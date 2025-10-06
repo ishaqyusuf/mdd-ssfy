9ed3530c0f853191d04393c7adae7caecda9686e
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-06 14:36:56 +0100

    .

--- apps/www/src/components/forms/sales-form/hpt/hpt-add-door-size.tsx
+++ apps/www/src/components/forms/sales-form/hpt/hpt-add-door-size.tsx
@@ -1,14 +1,19 @@
 import { openDoorSizeSelectModal } from "@/app/(clean-code)/(sales)/sales-book/(form)/_components/modals/door-size-select-modal/open-modal";
 import { Button } from "@gnd/ui/button";
 import { Icons } from "@gnd/ui/custom/icons";
+import { useHpt } from "../context";
+import { ComponentHelperClass } from "@/app/(clean-code)/(sales)/sales-book/(form)/_utils/helpers/zus/step-component-class";
 
 export function HptAddDoorSize({ cls }) {
+    const ctx = useHpt();
     const addDoorSize = () => {
+        // const s = ctx.hpt.getDoorStepForm2();
+        // console.log(s.);
         openDoorSizeSelectModal(cls);
     };
     return (
         <>
-            <Button disabled onClick={addDoorSize}>
+            <Button onClick={addDoorSize}>
                 <Icons.add className="mr-2 size-4" />
                 <span>Size</span>
             </Button>
