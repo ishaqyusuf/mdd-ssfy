8dd03c58359de73cdbf38ea198e8a56e2c8d4a25
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-10 08:01:05 +0100

    .

--- apps/www/src/lib/open-link.ts
+++ apps/www/src/lib/open-link.ts
@@ -1,13 +1,13 @@
 "use client";
-import { env } from "@/env.mjs";
 import QueryString from "qs";
 
-export function openLink(path, query = {}, newTab = false) {
+export function openLink(path, query = null, newTab = false) {
     const link = document.createElement("a");
     // let base = env.NEXT_PUBLIC_APP_URL;
     // base = path?.startsWith("http") ? null : `${base}/`;
     let base = path?.startsWith("http") ? "" : `${window.location.origin}/`;
     if (newTab) link.target = "_blank";
-    link.href = `${base || ""}${path}?${QueryString.stringify(query)}`;
+    const qs = query ? `?${QueryString.stringify(query)}` : null;
+    link.href = `${base || ""}${path}${qs}`;
     link.click();
 }
