5ca1d1d5b3c59119d47e91dd4c19c51bcd4ae1b2
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-10 09:40:59 +0100

    .

--- packages/ui/src/components/controls/form-input.tsx
+++ packages/ui/src/components/controls/form-input.tsx
@@ -92,7 +92,7 @@ export function FormInput<
                   />
                 </div>
               ) : qtyInputProps ? (
-                <>
+                <div>
                   <QuantityInput
                     onChange={field.onChange}
                     onFocus={() => setIsFocused(true)}
@@ -109,7 +109,7 @@ export function FormInput<
                       <div className="h-full w-full bg-[repeating-linear-gradient(-60deg,#DBDBDB,#DBDBDB_1px,transparent_1px,transparent_5px)] dark:bg-[repeating-linear-gradient(-60deg,#2C2C2C,#2C2C2C_1px,transparent_1px,transparent_5px)]" />
                     </div>
                   )}
-                </>
+                </div>
               ) : (
                 <div
                   className={cn(
