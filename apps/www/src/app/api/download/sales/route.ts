0eb9fec78690ce98d53ed04a42d788f2a30b51c7
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-22 09:52:17 +0100

    ..

--- apps/www/src/app/api/download/sales/route.ts
+++ apps/www/src/app/api/download/sales/route.ts
@@ -17,51 +17,49 @@ const paramsSchema = z.object({
 });
 // export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
-    return Response.json({});
-    // const requestUrl = new URL(req.url);
-    // // const session = await getServerSession(authOptions);
-    // const result = paramsSchema.safeParse(
-    //     Object.fromEntries(requestUrl.searchParams.entries())
-    // );
-    // const payload = await validateTokenAction(
-    //     result.data.token,
-    //     "salesPdfToken"
-    // );
+    const requestUrl = new URL(req.url);
+    // const session = await getServerSession(authOptions);
+    const result = paramsSchema.safeParse(
+        Object.fromEntries(requestUrl.searchParams.entries())
+    );
+    const payload = await validateTokenAction(
+        result.data.token,
+        "salesPdfToken"
+    );
 
-    // if (!payload) notFound();
-    // notFound();
-    // const printData = await generateLegacyPrintData(db, payload);
+    if (!payload) notFound();
+    const printData = await generateLegacyPrintData(db, payload);
 
-    // const {
-    //     // id, token,
-    //     preview,
-    // } = result.data;
+    const {
+        // id, token,
+        preview,
+    } = result.data;
+    try {
+        const pages = printData.map((a) => a.pageData);
 
-    // const pages = printData.map((a) => a.pageData);
+        const stream = await renderToStream(
+            await PdfTemplate({
+                pages,
+                template: {
+                    size: "A4",
+                },
+            })
+        );
+    } catch (error) {}
+    return NextResponse.json({ data: "Testing Sentry Error...!", printData });
+    const title = printData.map((a) => a.orderNo).join("-");
+    // @ts-expect-error - stream is not assignable to BodyInit
+    const blob = await new Response(stream).blob();
 
-    // const stream = await renderToStream(
-    //     await PdfTemplate({
-    //         pages,
-    //         template: {
-    //             size: "A4",
-    //         },
-    //     })
-    // );
+    const headers: Record<string, string> = {
+        "Content-Type": "application/pdf",
+        "Cache-Control": "no-store, max-age=0",
+    };
 
-    // // return NextResponse.json({ data: "Testing Sentry Error...!", printData });
-    // const title = printData.map((a) => a.orderNo).join("-");
-    // // @ts-expect-error - stream is not assignable to BodyInit
-    // const blob = await new Response(stream).blob();
+    if (!preview) {
+        headers["Content-Disposition"] = `attachment; filename="${title}.pdf"`;
+    }
 
-    // const headers: Record<string, string> = {
-    //     "Content-Type": "application/pdf",
-    //     "Cache-Control": "no-store, max-age=0",
-    // };
-
-    // if (!preview) {
-    //     headers["Content-Disposition"] = `attachment; filename="${title}.pdf"`;
-    // }
-
-    // return new Response(blob, { headers });
+    return new Response(blob, { headers });
 }
 
