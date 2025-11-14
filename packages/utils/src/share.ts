81bfe544b8f0e712181e4ae6ae20af5a061b6e06
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-11-14 17:01:12 +0100

    wip

--- packages/utils/src/share.ts
+++ packages/utils/src/share.ts
@@ -4,29 +4,31 @@ interface Props {
   msg: string;
 }
 export async function share(props: Props) {
+  let file;
   try {
     const blob = await fetch(props.url).then((r) => r.blob());
-    const file = new File([blob], "invoice.pdf", {
+    file = new File([blob], "invoice.pdf", {
       type: "application/pdf",
     });
-    console.log("FILE>>>");
-    if (navigator.share) {
-      await navigator.share({
-        title: "Invoice",
-        text: "Here’s your invoice",
-        files: [file],
-      });
-    } else {
-      //   alert("Sharing not supported on this device.");
-      const msg = `Here's your invoice: ${props.url}`;
-      const whatsappUrl = `https://wa.me?text=${encodeURIComponent(msg)}`;
-      const link = document.createElement("a");
-      link.target = "_blank";
-      link.href = whatsappUrl;
-      link.click();
-    }
+
     return;
   } catch (error) {}
+  console.log("FILE>>>");
+  if (navigator.share && file) {
+    await navigator.share({
+      title: "Invoice",
+      text: "Here’s your invoice",
+      files: [file],
+    });
+  } else {
+    //   alert("Sharing not supported on this device.");
+    const msg = `Here's your invoice: ${props.url}`;
+    const whatsappUrl = `https://wa.me?text=${encodeURIComponent(msg)}`;
+    const link = document.createElement("a");
+    link.target = "_blank";
+    link.href = whatsappUrl;
+    link.click();
+  }
   //   const msg = `Here's your invoice: ${props.url}`;
   //   const whatsappUrl = `https://wa.me?text=${encodeURIComponent(msg)}`;
   //   const link = document.createElement("a");
