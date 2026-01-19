deca2548df444ca2ea4769a15c3f2ffc126eac0d
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2026-01-19 21:14:45 +0100

    wip

--- apps/www/src/app/api/download-app/route.ts
+++ apps/www/src/app/api/download-app/route.ts
@@ -1,34 +1,27 @@
 // app/api/download/route.ts
 import { NextResponse } from "next/server";
 
+export const runtime = "nodejs"; // 👈 REQUIRED
+
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
+
     const fileUrl =
         searchParams.get("url") ??
         "https://expo.dev/artifacts/eas/2FnxTdEff3pRrfULnr3JXa.apk";
-    const filename = searchParams.get("name") ?? "GND Millwork.apk";
 
-    if (!fileUrl) {
-        return NextResponse.json({ error: "Missing url" }, { status: 400 });
-    }
+    const filename = searchParams.get("name") ?? "GND-Millwork.apk";
 
-    const res = await fetch(fileUrl, {
-        headers: {
-            Accept: "application/octet-stream",
-        },
-    });
-    // return NextResponse.json({});
+    const res = await fetch(fileUrl);
 
-    if (!res.ok) {
+    if (!res.ok || !res.body) {
         return NextResponse.json(
             { error: "Failed to fetch file" },
             { status: 400 }
         );
     }
 
-    const buffer = await res.arrayBuffer();
-
-    return new NextResponse(buffer, {
+    return new NextResponse(res.body, {
         headers: {
             "Content-Type": "application/vnd.android.package-archive",
             "Content-Disposition": `attachment; filename="${filename}"`,
