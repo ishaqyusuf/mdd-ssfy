27e761181fa9324b94b3eac998cf80249c8f8e25
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2026-03-28 17:55:48 +0100

    wip

--- apps/www/src/app/(sidebar)/community/(main)/layout.tsx
+++ apps/www/src/app/(sidebar)/community/(main)/layout.tsx
@@ -1,10 +1,12 @@
 import CommunitySummaryWidgets from "@/components/widgets/community-summary-widgets";
-
+import { CommunityTabs } from "@/components/community-tabs";
 export default async function Layout({ children }) {
     return (
         <div className="flex flex-col gap-6">
             <CommunitySummaryWidgets />
+            <CommunityTabs />
             {children}
         </div>
     );
 }
+
--- apps/www/src/app/(sidebar)/community/layout.tsx
+++ apps/www/src/app/(sidebar)/community/layout.tsx
@@ -1,10 +1,4 @@
-import { CommunityTabs } from "@/components/community-tabs";
-
 export default async function Layout({ children }) {
-    return (
-        <div className="pt-6 flex flex-col gap-6">
-            <CommunityTabs />
-            {children}
-        </div>
-    );
+    return <div className="pt-6 flex flex-col gap-6">{children}</div>;
 }
+
