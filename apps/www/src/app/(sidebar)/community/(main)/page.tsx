9a3064b6d74e021a66d8ed61c6ae7eed6ada2527
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2026-03-31 13:44:06 +0100

    wip

--- apps/www/src/app/(sidebar)/community/(main)/page.tsx
+++ apps/www/src/app/(sidebar)/community/(main)/page.tsx
@@ -6,23 +6,24 @@ import { unstable_noStore } from "next/cache";
 
 import PageShell from "@/components/page-shell";
 export async function generateMetadata(props) {
-	return constructMetadata({
-		title: "Community Dashboard | GND",
-	});
+    return constructMetadata({
+        title: "Community Dashboard | GND",
+    });
 }
 
 export default async function Page() {
-	unstable_noStore();
-	batchPrefetch([trpc.community.communityDashboardOverview.queryOptions({})]);
+    // unstable_noStore();
+    batchPrefetch([trpc.community.communityDashboardOverview.queryOptions({})]);
 
-	return (
-		<PageShell>
-			<div className="flex flex-col gap-6 px-4 sm:px-6">
-				<PageTitle>Community Dashboard</PageTitle>
-				<HydrateClient>
-					<CommunityDashboard />
-				</HydrateClient>
-			</div>
-		</PageShell>
-	);
+    return (
+        <PageShell>
+            <div className="flex flex-col gap-6 px-4 sm:px-6">
+                <PageTitle>Community Dashboard</PageTitle>
+                <HydrateClient>
+                    <CommunityDashboard />
+                </HydrateClient>
+            </div>
+        </PageShell>
+    );
 }
+
