33e1f2e444713343e08e25bd9fcf293c89beb9af
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2026-03-31 13:51:17 +0100

    wip

--- apps/www/src/app/(sidebar)/community/(main)/page.tsx
+++ apps/www/src/app/(sidebar)/community/(main)/page.tsx
@@ -1,11 +1,14 @@
 import { CommunityDashboard } from "@/components/community-dashboard";
 import { constructMetadata } from "@/lib/(clean-code)/construct-metadata";
-import { HydrateClient, batchPrefetch, trpc } from "@/trpc/server";
+import { batchPrefetch, trpc } from "@/trpc/server";
 import { PageTitle } from "@gnd/ui/custom/page-title";
-import { unstable_noStore } from "next/cache";
 
 import PageShell from "@/components/page-shell";
-export async function generateMetadata(props) {
+import { Suspense } from "react";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import { TableSkeleton } from "@/components/tables/skeleton";
+import { ErrorFallback } from "@/components/error-fallback";
+export async function generateMetadata() {
     return constructMetadata({
         title: "Community Dashboard | GND",
     });
@@ -19,9 +22,11 @@ export default async function Page() {
         <PageShell>
             <div className="flex flex-col gap-6 px-4 sm:px-6">
                 <PageTitle>Community Dashboard</PageTitle>
-                <HydrateClient>
-                    <CommunityDashboard />
-                </HydrateClient>
+                <ErrorBoundary errorComponent={ErrorFallback}>
+                    <Suspense fallback={<TableSkeleton />}>
+                        <CommunityDashboard />
+                    </Suspense>
+                </ErrorBoundary>
             </div>
         </PageShell>
     );
