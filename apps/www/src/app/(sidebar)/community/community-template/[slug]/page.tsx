cea876f60f2a41b60f43c4f490cc0104bcec0662
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2026-03-31 14:29:01 +0100

    wip

--- apps/www/src/app/(sidebar)/community/community-template/[slug]/page.tsx
+++ apps/www/src/app/(sidebar)/community/community-template/[slug]/page.tsx
@@ -1,48 +1,89 @@
-import { Breadcrumbs } from "@/components/_v1/breadcrumbs";
-import { BreadLink } from "@/components/_v1/breadcrumbs/links";
+// import { Breadcrumbs } from "@/components/_v1/breadcrumbs";
+// import { BreadLink } from "@/components/_v1/breadcrumbs/links";
 
-import { DataPageShell } from "@/components/_v1/shells/data-page-shell";
-import { transformCommunityTemplate } from "@/lib/community/community-template";
-import type { Metadata } from "next";
+// import { DataPageShell } from "@/components/_v1/shells/data-page-shell";
+// import { transformCommunityTemplate } from "@/lib/community/community-template";
+// import type { Metadata } from "next";
 
-import { getCommunityTemplate } from "@/app-deps/(v1)/(loggedIn)/settings/community/_components/home-template";
-import ModelForm from "@/app-deps/(v1)/(loggedIn)/settings/community/_components/model-form/model-form";
-import { InstallCostSidebar } from "@/components/install-cost-sidebar";
-import { PageTitle } from "@gnd/ui/custom/page-title";
-import { redirect } from "next/navigation";
+// import { getCommunityTemplate } from "@/app-deps/(v1)/(loggedIn)/settings/community/_components/home-template";
+// import ModelForm from "@/app-deps/(v1)/(loggedIn)/settings/community/_components/model-form/model-form";
+// import { InstallCostSidebar } from "@/components/install-cost-sidebar";
+// import { PageTitle } from "@gnd/ui/custom/page-title";
+
+// import PageShell from "@/components/page-shell";
+// import { redirect } from "next/navigation";
+// export const metadata: Metadata = {
+//     title: "Edit Community Template",
+// };
+
+// export default async function Page(props) {
+//     const params = await props.params;
+//     // redirect(`/community/community-template/${params.slug}/v1`);
+//     // const response: any = await getCommunityTemplate(params.slug);
+//     // if (response.meta?.design) {
+//     //     response.meta.design = transformCommunityTemplate(response.meta.design);
+//     // }
+//     // return (
+//     //     <PageShell>
+//     //         <DataPageShell
+//     //             data={{
+//     //                 community: true,
+//     //             }}
+//     //             className="space-y-4 p-8"
+//     //         >
+//     //             <PageTitle>{response?.modelName}</PageTitle>
+
+//     //             <ModelForm
+//     //                 title={
+//     //                     <div className="">
+//     //                         <span>Edit Community Model</span>
+//     //                     </div>
+//     //                 }
+//     //                 data={response as any}
+//     //             />
+//     //         </DataPageShell>
+//     //         <InstallCostSidebar />
+//     //     </PageShell>
+//     // );
+// }
+
+import { ErrorFallback } from "@/components/error-fallback";
+import { CommunityTemplateV1Form } from "@/components/forms/community-template-v1";
+import { HydrateClient, batchPrefetch } from "@/trpc/server";
+import { Skeletons } from "@gnd/ui/custom/skeletons";
+import { constructMetadata } from "@gnd/utils/construct-metadata";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import type { SearchParams } from "nuqs";
+import { Suspense } from "react";
 
 import PageShell from "@/components/page-shell";
-export const metadata: Metadata = {
-	title: "Edit Community Template",
+export const metadata = constructMetadata({
+    title: "Edit Community Template | GND",
+});
+
+type Props = {
+    searchParams: Promise<SearchParams>;
+    params: Promise<{
+        slug: string;
+    }>;
 };
 
-export default async function Page(props) {
-	const params = await props.params;
-	return redirect(`/community/community-template/${params.slug}/v1`);
-	const response: any = await getCommunityTemplate(params.slug);
-	if (response.meta?.design) {
-		response.meta.design = transformCommunityTemplate(response.meta.design);
-	}
-	return (
-		<PageShell>
-			<DataPageShell
-				data={{
-					community: true,
-				}}
-				className="space-y-4 p-8"
-			>
-				<PageTitle>{response?.modelName}</PageTitle>
+export default async function Page(props: Props) {
+    const { slug } = await props.params;
 
-				<ModelForm
-					title={
-						<div className="">
-							<span>Edit Community Model</span>
-						</div>
-					}
-					data={response as any}
-				/>
-			</DataPageShell>
-			<InstallCostSidebar />
-		</PageShell>
-	);
+    batchPrefetch([]);
+    return (
+        <PageShell>
+            <HydrateClient>
+                <div className="flex flex-col gap-6">
+                    <ErrorBoundary errorComponent={ErrorFallback}>
+                        <Suspense fallback={<Skeletons.Dashboard />}>
+                            <CommunityTemplateV1Form slug={slug} />
+                        </Suspense>
+                    </ErrorBoundary>
+                </div>
+            </HydrateClient>
+        </PageShell>
+    );
 }
+
